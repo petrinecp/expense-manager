da01ab00970bc1cc3fcdbdb15dd33db61999bbae
Martin Drimal <373769@mail.muni.cz>
2015-06-21 11:33:00 +0200

    Fix REST test for User and Category

--- src/test/java/cz/muni/expense/rest/UserResourceRESTServiceTest.java
+++ src/test/java/cz/muni/expense/rest/UserResourceRESTServiceTest.java
@@ -42,60 +42,12 @@ public class UserResourceRESTServiceTest {
         return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addPackages(true, "cz.muni.expense")
                 .addAsResource("META-INF/our-persistence.xml", "META-INF/persistence.xml")
-                .addAsResource("META-INF/test-import.sql", "import.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
                 .addAsWebInfResource("test-ds.xml");
     }
     
     @Test
     @InSequence(1)
-    public void listAllTest() throws Exception {
-
-        Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/test/rest/user");
-        Response response = target.request(MediaType.APPLICATION_JSON).get();
-        assertEquals(200, response.getStatus());
-
-        List<User> users = objectParser.getObjectFromJson(response.readEntity(String.class));
-        assertEquals("Database should contains two users.", 2, users.size());
-        assertEquals("Wrong user name", "Carl", users.get(0).getName());
-        assertEquals("Wrong user forname", "Hostings", users.get(0).getForname());
-        assertEquals("Wrong user name", "Peter", users.get(1).getName());
-        assertEquals("Wrong user forname", "Nash", users.get(1).getForname());        
-
-        response.close();
-    }
-	
-    @Test
-    @InSequence(2)
-    public void lookupByIdTest() throws Exception {
-        Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/test/rest/user/0");
-        Response response = target.request(MediaType.APPLICATION_JSON).get();
-        assertEquals(200, response.getStatus());
-
-        List<User> users = objectParser.getObjectFromJson(response.readEntity(String.class));
-        assertEquals("Database should contains two users.", 1, users.size());
-        assertEquals("Wrong user name", "Carl", users.get(0).getName());
-        assertEquals("Wrong user forname", "Hostings", users.get(0).getForname());
-
-        response.close();
-    }
-	
-    @Test
-    @InSequence(3)
-    public void lookupByNonExistIdTest() throws Exception {
-        Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/test/rest/user/20");
-        Response response = target.request(MediaType.APPLICATION_JSON).get();
-        assertEquals(404, response.getStatus());
-
-        response.close();
-    }
-    
-    //TODO analyze error test
-    @Test
-    @InSequence(4)
     public void createTest() throws Exception {
         User user = new User();
         user.setName("Adam");
@@ -105,12 +57,55 @@ public class UserResourceRESTServiceTest {
         Response response = target.request(MediaType.APPLICATION_JSON).post(Entity.json(user));
 
         assertEquals("Response should be created.", 201, response.getStatus());
-        //assertEquals("c", response.readEntity(String.class));
         List<User> users = objectParser.getObjectFromJson(response.readEntity(String.class));
         assertEquals("After put should be retrieved just 1 object", 1, users.size());
         assertEquals("Wrong user name.", "Adam", users.get(0).getName());
         assertEquals("Wrong user forname.", "Eagel", users.get(0).getForname());
-        assertEquals("Wrong user id.", 3, users.get(0).getId().intValue());
+        assertEquals("Wrong user id.", 1, users.get(0).getId().intValue());
+
+        response.close();
+    }
+    
+    @Test
+    @InSequence(2)
+    public void listAllTest() throws Exception {
+
+        Client client = ClientBuilder.newBuilder().build();
+        WebTarget target = client.target("http://localhost:8080/test/rest/user");
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        assertEquals(200, response.getStatus());
+
+        List<User> users = objectParser.getObjectFromJson(response.readEntity(String.class));
+        assertEquals("Database should contains two users.", 1, users.size());
+        assertEquals("Wrong user name", "Adam", users.get(0).getName());
+        assertEquals("Wrong user forname", "Eagel", users.get(0).getForname());      
+
+        response.close();
+    }
+	
+    @Test
+    @InSequence(3)
+    public void lookupByIdTest() throws Exception {
+        Client client = ClientBuilder.newBuilder().build();
+        WebTarget target = client.target("http://localhost:8080/test/rest/user/1");
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        assertEquals(200, response.getStatus());
+
+        List<User> users = objectParser.getObjectFromJson(response.readEntity(String.class));
+        assertEquals("Database should contains two users.", 1, users.size());
+        assertEquals("Wrong user name", "Adam", users.get(0).getName());
+        assertEquals("Wrong user forname", "Eagel", users.get(0).getForname());
+
+        response.close();
+    }
+	
+    @Test
+    @InSequence(4)
+    public void lookupByNonExistIdTest() throws Exception {
+        Client client = ClientBuilder.newBuilder().build();
+        WebTarget target = client.target("http://localhost:8080/test/rest/user/20");
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        assertEquals(404, response.getStatus());
 
         response.close();
     }
@@ -141,7 +136,7 @@ public class UserResourceRESTServiceTest {
     @InSequence(6)
     public void deleteByIdTest() {
         Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/test/rest/user/0");
+        WebTarget target = client.target("http://localhost:8080/test/rest/user/1");
         Response response = target.request().delete();
         assertEquals("Bank should be deleted", 200, response.getStatus());
     }
