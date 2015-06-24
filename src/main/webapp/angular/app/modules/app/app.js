882d610ed10067205385dcf7805216362c9169a4
Lukáš Valach <lukas.valach@unicornsystems.eu>
2015-06-25 00:35:00 +0200

    WebSocket fixed and some minor changes.

--- src/main/webapp/angular/app/modules/app/app.js
+++ src/main/webapp/angular/app/modules/app/app.js
@@ -8,6 +8,7 @@ var app = angular.module('app', [
     'ngResource',
     'ui.router',
     'ui.bootstrap',
+    'ngWebSocket',
     'item',
     'item-new',
     'items',
@@ -45,7 +46,7 @@ var app = angular.module('app', [
 	    $httpProvider.interceptors.push('httpErrorInterceptor');
 	})
 
-    .controller('app', ['$rootScope', '$scope', '$state', 'authFactory', 'GeneralRestService', function ($rootScope, $scope, $state, authFactory, GeneralRestService) {
+    .controller('app', ['$rootScope', '$scope', '$state', 'authFactory', 'GeneralRestService', 'webSocketFactory', function ($rootScope, $scope, $state, authFactory, GeneralRestService, webSocketFactory) {
         'use strict';
         
         $scope.$state = $state;
@@ -58,11 +59,23 @@ var app = angular.module('app', [
         };
         $scope.data = GeneralRestService;
         
+        $scope.webSocketFactory = webSocketFactory;
+
+        $scope.sendWebSocket = function (message) {
+        	var userName = authFactory.getUserName();
+    		var authToken = authFactory.getAuthData().authToken;
+    		webSocketFactory.send("{\"timestamp\":\"" + new Date() + "\",\"userId\":\""+userName+"\",\"token\":\""+authToken+"\",\"action\":\"Click on login button\"}");
+        };     		
+		
         $scope.login = function (authLoginElement) {
             authFactory.login(authLoginElement).success(function (data) {
                 authFactory.setAuthData(data);
                 $rootScope.user = data;
                 $scope.loggedIn = true;
+                $scope.sendWebSocket();
+                if ($rootScope.user.image === undefined || $rootScope.user.image == null){
+                	$rootScope.user.image = "img/blank.jpg";
+                }
                 $state.go('payments');
             }).error(function () {
             	$scope.loggedIn = false;
@@ -254,16 +267,10 @@ var app = angular.module('app', [
         };
         
         authFactory.login = function (authLoginElement) {
-            var webSocket = new WebSocket('wss://localhost:8443/expense-manager/websocket');
-    		webSocket.send("{\"timestamp\":"+new Date()+",\"userId\":2,\"token\":\"test\",\"action\":\"Click on login button\"}");
-    		
             return $http.post('rest/auth/login', authLoginElement);
         };
 		
 		authFactory.logout = function () {
-			var webSocket = new WebSocket('wss://localhost:8443/expense-manager/websocket');
-    		webSocket.send("{\"timestamp\":"+new Date()+",\"userId\":2,\"token\":\"test\",\"action\":\"Click on logout button\"}");
-    		
             return $http.post('rest/auth/logout');
         };
 
@@ -296,81 +303,54 @@ var app = angular.module('app', [
     	return errorHttpRequestInterceptor;
     }])
     
-//    .factory('Data', ['$rootScope', '$resource', function ($rootScope, $resource) {
-//        'use strict';
-//        return {
-//            resource: $resource('data/:section.json', null, {
-//                get: { method: 'GET', isArray: false, params: { section: '@section', id: '@id' }, url: 'data/:section/:id.json' },
-//                post: { method: 'POST', isArray: false, params: { section: '@section', id: '@id' }, url: 'data/:section/:id.json' },
-//                query: { method: 'GET', isArray: true, params: { section: '@section', id: '@id' } }
-//            }),
-//            /**
-//             * @method get
-//             * Single item
-//             */
-//            get: function (params, callback, error) {
-//                var me = this;
-//                //console.log('get', params);
-//                me.resource.get(params, null, function (value) {
-//                    me.setData(value, params.section, params.id);
-//                    if (callback) { callback(value); }
-//                }, error);
-//            },
-//            /**
-//             * @method post
-//             * Submit data
-//             */
-//            post: function (params, callback, error) {
-//                var me = this;
-//                //console.log('post', params);
-//                me.resource.post(params, me.getData(params.section, params.id), function (value) {
-//                    //me.setData(value, params.section, params.id); // do not save post data
-//                    if (callback) { callback(value); }
-//                }, error);
-//            },
-//            /**
-//             * @method query
-//             * List of items
-//             */
-//            query: function (params, callback, error) {
-//                var me = this;
-//                //console.log('query', params);
-//                me.resource.query(params, null, function (value) {
-//                    me.setData(value, params.section, params.id);
-//                    if (callback) { callback(value); }
-//                }, error);
-//            },
-//            /**
-//             * @method setData
-//             * Cache the items for sharing between controllers
-//             */
-//            setData: function (value, section, id) {
-//                if (typeof id === 'number') {
-//                    if (!this[section + '_detail']) {
-//                        this[section + '_detail'] = {};
-//                    }
-//                    this[section + '_detail'][id] = value;
-//                } else {
-//                    this[section] = value;
-//                }
-//            },
-//            /**
-//             * @method getData
-//             * Get cached data object
-//             */
-//            getData: function (section, id) {
-//                if (typeof id === 'number') {
-//                    if (id === 0) {
-//                        return this[section + '_new'];
-//                    } else {
-//                        return this[section + '_detail'][id];
-//                    }
-//                } else {
-//                    return this[section];
-//                }
-//            }
-//        };
-//    }])
+
+    .factory(
+		'webSocketFactory',
+		function($websocket, $log, $injector) {
+			// Open a WebSocket connection
+			var ws = $websocket("wss://" + document.location.host + "/expense-manager/websocket");
+			var wsc = [];
+
+			ws.onMessage(function(event) {
+				$log.info('message: ', event.data);
+				var response;
+				try {
+					response = angular.fromJson(event.data);
+				} catch (e) {
+					$log.error('error: ', e);
+					response = {
+						'error' : e
+					};
+				}
+			});
+			ws.onError(function(event) {
+				$log.error('connection Error', event);
+			});
+			ws.onClose(function(event) {
+				$log.info('connection closed', event);
+			});
+			ws.onOpen(function() {
+				$log.info('connection open');
+				var authFactory = $injector.get('authFactory');
+				var userName = authFactory.getUserName();
+				var authToken = authFactory.getAuthData().authToken;
+				ws.send("{\"timestamp\":\"" + new Date() + "\",\"userId\":\""+userName+"\",\"token\":\""+authToken+"\",\"action\":\"New webSocket connection opened.\"}");
+			});
+
+			return {
+				wsc : wsc,
+				status : function() {
+					return ws.readyState;
+				},
+				send : function(message) {
+					if (angular.isString(message)) {
+						ws.send(message);
+					} else if (angular.isObject(message)) {
+						ws.send(JSON.stringify(message));
+					}
+				}
+			};
+		})
 
     .filter('trusted', ['$sce', function ($sce) {
         'use strict';
@@ -392,63 +372,4 @@ var app = angular.module('app', [
             }
         };
     })
-
-//    .directive('focusPoint', function () {
-//        'use strict';
-//        
-//        return {
-//            restrict: 'A',
-//            replace: true,
-//            scope: {
-//                model: '=ngModel'
-//            },
-//            template: '<div class="focus-point">' +
-//                        '<div class="focus-area">' +
-//                            '<span class="target" style="left: {{ x }}%; top: {{ y }}%"></span>' +
-//                            '<img src="{{ src }}" alt="" ng-mousemove="onMouseMove($event)" ng-mousedown="onMouseDown($event)" ng-mouseup="onMouseUp($event)" draggable="false" class="source" />' +
-//                        '</div>' +
-//                        '<span style="background-image: url(\'{{ src }}\'); background-position: {{ x }}% {{ y }}%" class="preview-portrait"></span>' +
-//                        '<span style="background-image: url(\'{{ src }}\'); background-position: {{ x }}% {{ y }}%" class="preview"></span>' +
-//                        '<span style="background-image: url(\'{{ src }}\'); background-position: {{ x }}% {{ y }}%" class="preview-landscape"></span>' +
-//                        '<span style="background-image: url(\'{{ src }}\'); background-position: {{ x }}% {{ y }}%" class="preview-wide"></span>' +
-//                        '<div class="info">{{ x }}% / {{ y }}%</div>' +
-//                       '</div>',
-//            link: function (scope, element, attr) {
-//                var dragging = false;
-//                scope.src = attr.src;
-//
-//                scope.onMouseDown = function (e) {
-//                    scope.update(e);
-//                    dragging = true;
-//                };
-//
-//                scope.onMouseMove = function (e) {
-//                    if (dragging === true) {
-//                        scope.update(e);
-//                    }
-//                };
-//
-//                scope.onMouseUp = function (e) {
-//                    e.preventDefault();
-//                    dragging = false;
-//                };
-//                
-//                scope.update = function (e) {
-//                    e.preventDefault();
-//                    var offset = scope.offset(e.target);
-//                    scope.x = Math.round(((e.pageX - offset.left) / e.target.clientWidth) * 100);
-//                    scope.y = Math.round(((e.pageY - offset.top) / e.target.clientHeight) * 100);
-//                };
-//                
-//                scope.offset = function (elm) {
-//                    try { return elm.offset(); } catch (e) { }
-//                    var body = document.documentElement || document.body;
-//                    return {
-//                        left: elm.getBoundingClientRect().left + (window.pageXOffset || body.scrollLeft),
-//                        top: elm.getBoundingClientRect().top + (window.pageYOffset || body.scrollTop)
-//                    };
-//                };
-//            }
-//        };
-//    })
-    ;
+;
